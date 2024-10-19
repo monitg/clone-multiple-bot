a6b1e7f55cbed95903c22ffba429cb94ebefa22a
monitg <142651228+monitg@users.noreply.github.com>
2024-10-19 17:26:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://monitgmoni:MoniTG007@cluster0.c6zfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MDL-Filter-bot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MDL-Filterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MDL-Collection')
 
 # Premium And Referal Settings
